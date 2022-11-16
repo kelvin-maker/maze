2f3905fed8634761a913a9373f856429844f1583
Kelvin Mwenda <NKMKE1964@gmail.com>
2022-11-16 09:49:00 +0300

    first commit

---
+++ sdl-start/build/CMakeFiles/sdl-start.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdl-start.dir/src/main.cpp.o"
+  "CMakeFiles/sdl-start.dir/src/main.cpp.o.d"
+  "sdl-start"
+  "sdl-start.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdl-start.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sdl-start/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/art/PROJECTX/sdl-start
+# Build directory: /home/art/PROJECTX/sdl-start/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
